0fd2537883828b13e3f5f9e829c82ddedc01c53b
afrear <amfrear1@gmail.com>
2024-10-22 19:45:18 -0700

    Added Activity 1 Part 2 Screenshots

---
+++ ASPCoreFirstApp/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreFirstApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string name, int personality = 9)
+        {
+            ViewData["Name"] = name;
+            ViewData["Personality"] = personality;
+
+            return View();
+        }
+
+        public IActionResult Data(int OrderNumber, float Price, int qty)
+        {
+            return Json(new { OrderNumber = OrderNumber, Price = Price, Quantity = qty });
+        }
+    }
+}
