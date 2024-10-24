3302c60adfbc724def6c326867d210dcf0f6b400
afrear <amfrear1@gmail.com>
2024-10-24 12:26:45 -0700

    Moved Projects into their respective folders

---
+++ Milestone 1/MinesweeperMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinesweeperMVC.Models;
+
+namespace MinesweeperMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet for each model class you want to store in the database.
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Milestone 1/MinesweeperMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinesweeperMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
