0c1d158f1240bd7eac02b8ca7461604bcdd68235
afrear <amfrear1@gmail.com>
2024-10-25 15:51:35 -0700

    Deleted Activity 2 and Milestone Project

--- RegisterAndLoginApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RegisterAndLoginApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
