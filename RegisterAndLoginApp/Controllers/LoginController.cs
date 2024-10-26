183b89c1ac65015dd32126ea8b927e63e5a3c08d
afrear <amfrear1@gmail.com>
2024-10-26 03:13:32 -0700

    Activity 2 Initial Commit

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // Static instance of UserCollection to manage users
+        static UserCollection users = new UserCollection();
+
+        // GET: Login page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Process the login form
+        [HttpPost]
+        public IActionResult ProcessLogin(LoginViewModel loginViewModel)
+        {
+            if (loginViewModel.Username == null || loginViewModel.Password == null)
+            {
+                ViewBag.Message = "Username and Password are required.";
+                return View("Index");
+            }
+
+            var result = users.CheckCredentials(loginViewModel.Username, loginViewModel.Password);
+
+            if (result > 0)
+            {
+                var user = users.GetUserById(result);
+                return View("LoginSuccess", user);
+            }
+            else
+            {
+                return View("LoginFailure");
+            }
+        }
+    }
+}
---
+++ RegisterAndLoginApp/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? Salt { get; set; }
+    public string? Groups { get; set; }
+
+    public void SetPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            // Combine the salt and password for hashing
+            var saltBytes = Encoding.UTF8.GetBytes(Salt ?? string.Empty);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var combinedBytes = saltBytes.Concat(passwordBytes).ToArray();
+            var hashBytes = sha256.ComputeHash(combinedBytes);
+            PasswordHash = Convert.ToBase64String(hashBytes);
+        }
+    }
+
+    public bool VerifyPassword(string password)
+    {
+        if (PasswordHash == null) return false;
+
+        using (var sha256 = SHA256.Create())
+        {
+            // Hash the provided password with the same salt
+            var saltBytes = Encoding.UTF8.GetBytes(Salt ?? string.Empty);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var combinedBytes = saltBytes.Concat(passwordBytes).ToArray();
+            var hashBytes = sha256.ComputeHash(combinedBytes);
+            var computedHash = Convert.ToBase64String(hashBytes);
+
+            // Compare computed hash with stored hash
+            return computedHash == PasswordHash;
+        }
+    }
+}
