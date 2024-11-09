2d7e187414e92a852ce864102997e277805bb9c3
afrear <amfrear1@gmail.com>
2024-11-09 15:54:15 -0700

    Added AppointmentScheduler App to Activity 4 Directory (Activity4Part2)

---
+++ AppointmentScheduler/Controllers/AppointmentsController.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentScheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowAppointmentDetails(AppointmentModel appointment)
+        {
+            return View(appointment);
+        }
+    }
+}
